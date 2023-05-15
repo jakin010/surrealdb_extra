22fab4d2ee9f4c72e8b12d3b0edaa47b4678726c
Jakin Girma <jakin@vdmi.nl>
2023-05-15 17:14:16 +0200

    Added allowed names

--- surrealdb_orm/src/trait/table.rs
+++ surrealdb_orm/src/trait/table.rs
@@ -1,5 +1,8 @@
-pub use surrealdb_orm_derive::Table;
+pub use ::surrealdb_orm_derive::Table;
+
 
 pub trait Table {
     fn name() -> String;
+
+    fn id(&self) -> ::surrealdb::sql::Thing;
 }
--- surrealdb_orm_derive/src/lib.rs
+++ surrealdb_orm_derive/src/lib.rs
@@ -1,10 +1,10 @@
 extern crate proc_macro;
 
 
-use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput, Meta, Token};
-use syn::punctuated::Punctuated;
+use ::proc_macro::TokenStream;
+use ::quote::quote;
+use ::syn::{parse_macro_input, DeriveInput, Meta, Token, Expr, Lit};
+use ::syn::punctuated::Punctuated;
 
 #[proc_macro_derive(Table, attributes(table))]
 pub fn table(input: TokenStream) -> TokenStream {
@@ -13,12 +13,17 @@ pub fn table(input: TokenStream) -> TokenStream {
     // println!("input: {:#?}", input);
 
     let struct_name = &input.ident;
-    let collection_name = get_table_name(&input).unwrap();
+    let table_name = get_table_name(&input).unwrap();
+//    let table_id = get_table_id(&input).unwrap();
 
     let expanded = quote! {
         impl Table for #struct_name {
             fn name() -> String {
-                #collection_name.to_string()
+                #table_name.to_string()
+            }
+
+            fn id(&self) -> ::surrealdb::sql::Thing {
+                ::surrealdb::sql::Thing::from(("test", "test"))
             }
         }
     };
@@ -27,15 +32,36 @@ pub fn table(input: TokenStream) -> TokenStream {
 }
 
 fn get_table_name(input: &DeriveInput) -> Option<String> {
+    let attr_name = "name";
     for attr in &input.attrs {
         if attr.path().is_ident("table") {
             let nested = attr.parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated).unwrap();
-
             for meta in nested {
                 match meta {
-                    Meta::NameValue(nv) if nv.path.is_ident("name") => {
+                    Meta::NameValue(nv) if nv.path.is_ident(attr_name) => {
                         match nv.value {
-                            
+                            Expr::Lit(expr_lit) => {
+                                match expr_lit.lit {
+                                    Lit::Str(lit) => {
+                                        let v = lit.value();
+
+                                        if !v.chars().nth(0).unwrap().is_alphabetic() {
+                                            panic!("table({}) attribute first character must be alphabetic", attr_name);
+                                        }
+
+                                        if v.contains(char::is_whitespace) {
+                                            panic!("table({}) attribute must not have any whitespace", attr_name);
+                                        }
+
+                                        if !v.chars().all(|x| x.is_alphanumeric() || "_".contains(x)) {
+                                            panic!("table({}) attribute can only have alphanumeric and/or `_` characters", attr_name);
+                                        }
+
+                                        return Some(v);
+                                    }
+                                    _ => { return None; }
+                                }
+                            }
                             _ => { return None; }
                         }
                     }
